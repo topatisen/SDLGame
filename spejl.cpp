b9225a54183eafb89d2d06d822d1acea6e791e93
topatisen <johannes.fornaeus@gmail.com>
2014-04-28 16:53:05 +0200

    Endring

--- spejl.cpp
+++ spejl.cpp
@@ -4,56 +4,16 @@ using namespace std;
 
 //adding timer.h
 #include "timer.h"
+#include "utilities.h"
 
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 768
-
-//tjii
 
 const int SCREEN_FPS = 60;
 const int SCREEN_TICK_PER_FRAME = 1000 / SCREEN_FPS;
 
 
-/*
- * Log an SDL error
- * @param os Which strean to log to
- * @param msg The error message to write, format will be msg error: SDL_GetError()
- */
-void logSDLError(std::ostream &os, const std::string &msg) {
-	os << msg << " error:" << SDL_GetError() << std::endl;
-}
 
-/*
- * Loads a BMP image into a texture rendering device
- * @param file The BMP image to load
- * @param ren The rendered to use
- * @return the loaded texture or nullptr if something went wrong
- */
-
-SDL_Texture *loadTexture(const std::string &file, SDL_Renderer *ren) {
-	SDL_Texture *texture = nullptr;
-	SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
-
-	if(loadedImage != nullptr) {
-		texture = SDL_CreateTextureFromSurface(ren, loadedImage);
-		SDL_FreeSurface(loadedImage);
-		if(texture == nullptr)
-			logSDLError(std::cerr, "CreateTextureFromSurface");
-	} else 
-		logSDLError(std::cerr, "LoadBMP");
-
-	return texture;
-}
-
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y) {
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-
-	// Query the texture to get its width and heigth to use
-	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
-	SDL_RenderCopy(ren, tex, NULL, &dst);
-}
 
 int main(int argc, char *argv[]) {
 	SDL_Event e;
