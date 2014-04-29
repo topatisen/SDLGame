d949be5765550efad744245507d9045ff140124b
topatisen <johannes.fornaeus@gmail.com>
2014-04-29 13:39:24 +0200

    Changed name of spejl to main, some small things

--- utilities.cpp
+++ utilities.cpp
@@ -12,7 +12,10 @@ SDL_Texture *loadTexture(const std::string &file, SDL_Renderer *ren) {
 	SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
 
 	if(loadedImage != nullptr) {
+	
+		//transparency ffs! (Foer fasen!)
 		SDL_SetColorKey(loadedImage,true,SDL_MapRGB(loadedImage->format,255,0,255));
+		
 		texture = SDL_CreateTextureFromSurface(ren, loadedImage);
 		SDL_FreeSurface(loadedImage);
 		if(texture == nullptr)
